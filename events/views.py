e408bca6aba7497ea3e910a39ac08be67e8b2ea6
Manan Bhanushali <mananbhanushali999@gmail.com>
2021-03-04 09:00:19 +0530

    Add 'add_venue' Page to Add Venues without using Admin Area

--- events/views.py
+++ events/views.py
@@ -3,12 +3,32 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event
+from .forms import VenueForm
+from django.http import HttpResponseRedirect
+
+
+def add_venue(request):
+    submitted = False
+
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/add_venue?submitted=True")
+
+    else:
+        form = VenueForm
+        if "submitted" in request.GET:
+            submitted = True
+
+    return render(request, "events/add_venue.html", {"form": form, "submitted": submitted},)
 
 
 def all_events(request):
     event_list = Event.objects.all()
     return render(request, 'events/event_list.html',
                   {'event_list': event_list})
+
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime("%B")):
     name = "Manan"
